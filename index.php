9e64d65e3deec0ff532a57da9c8c2b8796c689a6
kaori6343 <52916201+kaori6343@users.noreply.github.com>
2019-07-16 22:04:58 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$siteTitle = '高校生バンド「lazy drops」Official Website';
+require('head.php');
+?>
+  <body id="main" class="index top-page">
+
+  <?php
+  require('header.php');
+   ?>
+
+<div class="container">
+  <div id="main-image">
+      <div id="image-area">
+        <img src="img/band.jpg" alt="Lazy Drop's" class="top-image">
+      </div>
+    </div>
+</div>
+  <?php
+  require('footer.php');
+   ?>
